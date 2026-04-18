f2e65dc94cb63741dcbb42f96aea63e73a3e7b04
Manickapream <131349455+Manickapream@users.noreply.github.com>
2026-04-19 00:08:42 +0530

    Remove frontend files for backend branch

--- pos_frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-export default function ProtectedRoute() {
-  const isAdmin = !!sessionStorage.getItem('admin_id')
-  return isAdmin ? <Outlet /> : <Navigate to="/login" replace />
-}
